2d3254a844cf56ccc57390d3e773cacbf09a2556
Soukaina Lakhmaiss <soukainalakhmaiss743@gmail.com>
2025-02-04 21:17:52 +0100

    not important files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-red-500">
       Hello world!
-    </h1>
+      </h1>
+      <Button className="bg-red-500" variant="ghost">Click me</Button>
     </div>
   );
 };
